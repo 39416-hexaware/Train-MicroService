45620f39dbe3e1171e32e94e01a39a94d066b4c7
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-01-10 12:30:38 +0530

    first draft

---
+++ README.md
@@ -0,0 +1,7 @@
+# Train Booking Bot MicroService
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://www.google.com)
+
+MicroServer for getting and processing data from API 
+
+  - Train Services
---
+++ index.js
@@ -0,0 +1,30 @@
+//imports
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+app = express();
+//Create express object
+
+var port = 7001;
+//Assign port
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//Configuring express app behaviour
+
+app.get("/api", function (req, res) {
+    res.send("Train MicroService works");
+});
+//GET Endpoint
+
+app.post("/api", function (req, res) {
+    console.log(JSON.stringify(req.body.result.action));
+
+    console.log('req.body.originalRequest.source');
+});
+//POST Call Endpoint
+
+console.log("Server Running at Port : " + port);
+
+app.listen(port);
