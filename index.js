cc79619bc0feea2536a1bbe74b97566525396d92
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-01-11 12:27:24 +0530

    package.json commit

--- index.js
+++ index.js
@@ -16,20 +16,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/PNRStatus", function (req, res) {
     res.send("PNRStatus MicroService works");
 });
-
-app.get("/FareDetails", function (req, res) {
-    res.send("FareDetails MicroService works");
-});
-
-app.get("/TrainRoute", function (req, res) {
-    res.send("TrainRoute MicroService works");
-});
 //GET Endpoint
 
 app.post("/RailwayAPI", function (req, res) {
-    console.log(JSON.stringify(req.body.result.action));
+    console.log(JSON.stringify(req.body));
 
-    console.log('req.body.originalRequest.source');
+    var header = {
+        'Cache-Control': 'no-cache',
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      };
+    
+    var options = {
+        url: 'https://api.railwayapi.com/v2/route/train/12671/apikey/sl5zmz3g1w', //,urlPath, //'https://api.railwayapi.com/v2/pnr-status/pnr/4338716830/apikey/sl5zmz3g1w'
+        method: 'GET',
+        header: header,
+        body: '',
+        json: true
+      };
+    
+    request(options, function (error, response, body) {
+        if (error) {
+          console.dir(error);
+          return
+        }
+        console.log('headers:' + response.headers);
+        console.log('status code:' + response.statusCode);
+          console.log(body);      
+      });
 });
 //POST Call Endpoint
 
